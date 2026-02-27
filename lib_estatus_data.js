06c750a5f59bc20d4c94e2294ca5741d4ff93587
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 09:29:57 -0600

    Update lib_estatus_data.js

--- lib_estatus_data.js
+++ lib_estatus_data.js
@@ -1,10 +1,11 @@
 const SatexEstatusData = {
     actualizar: function(hCont, hOe, hCon, tParadas) {
         const container = document.getElementById('data-husos-container');
+        
         const crearBloque = (label, valor, color = "#f9b218") => `
-            <div style="background:#3c546d; border-left:4px solid ${color}; padding:2px 12px; min-width:120px; height:40px; display:flex; flex-direction:column; justify-content:center;">
-                <span style="color:#a1b1c1; font-size:9px; font-weight:bold; font-family:sans-serif;">${label}</span>
-                <span style="color:${color}; font-size:20px; font-weight:bold; font-family:sans-serif; line-height:1;">${valor}</span>
+            <div style="background: #3c546d; border-left: 3px solid ${color}; padding: 2px 8px; min-width: 100px; height: 45px; display: flex; flex-direction: column; justify-content: center; box-sizing: border-box;">
+                <span style="color: #a1b1c1; font-size: 9px; font-weight: bold; font-family: sans-serif; white-space: nowrap;">${label}</span>
+                <span style="color: ${color}; font-size: 22px; font-weight: bold; font-family: sans-serif; line-height: 1;">${valor}</span>
             </div>`;
         
         container.innerHTML = 
