712fef9457ea90fd95a0ac7a5bebbea5c9fb5182
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 09:44:26 -0600

    Update lib_estatus_data.js

--- lib_estatus_data.js
+++ lib_estatus_data.js
@@ -2,9 +2,9 @@ const SatexEstatusData = {
     actualizar: function(hCont, hOe, hCon, mParadas) {
         const container = document.getElementById('data-husos-container');
         const crearBloque = (label, valor, color = "#f9b218") => `
-            <div style="background: #3c546d; border-left: 3px solid ${color}; padding: 2px 10px; min-width: 105px; height: 40px; display: flex; flex-direction: column; justify-content: center; box-sizing: border-box;">
-                <span style="color: #a1b1c1; font-size: 9px; font-weight: bold; font-family: sans-serif; white-space: nowrap;">${label}</span>
-                <span style="color: ${color}; font-size: 20px; font-weight: bold; font-family: sans-serif; line-height: 1;">${valor}</span>
+            <div style="background: #3c546d; border-left: 3px solid ${color}; padding: 0 10px; min-width: 100px; height: 36px; display: flex; flex-direction: column; justify-content: center; box-sizing: border-box;">
+                <span style="color: #a1b1c1; font-size: 8px; font-weight: bold; font-family: sans-serif; white-space: nowrap; margin-bottom: -2px;">${label}</span>
+                <span style="color: ${color}; font-size: 18px; font-weight: bold; font-family: sans-serif; line-height: 1;">${valor}</span>
             </div>`;
         
         container.innerHTML = 
