16b1748542d25a29d4f627c557ef1b67c1943288
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 08:07:27 -0600

    Update lib_estatus.js

--- lib_estatus.js
+++ lib_estatus.js
@@ -1,28 +1,12 @@
 const SatexEstatus = {
     dibujar: function(id) {
         document.getElementById(id).innerHTML = `
-            <div class="estatus-seccion">
-                <div class="etiqueta-principal">
-                    Estatus planta<br>
-                    <span style="font-size:11px; font-weight:normal; opacity:0.8;">Info. Cardas ▶</span>
-                </div>
-                <div class="bloque-dato">
-                    <span class="dato-titulo">Continuas (Inact.)</span>
-                    <span class="dato-valor" id="h-cont">0</span>
-                </div>
-                <div class="bloque-dato">
-                    <span class="dato-titulo">Open-end (Inact.)</span>
-                    <span class="dato-valor" id="h-oe">0</span>
-                </div>
-                <div class="bloque-dato">
-                    <span class="dato-titulo">Coneras (Inact.)</span>
-                    <span class="dato-valor" id="h-con">0</span>
-                </div>
-                <div class="bloque-dato" style="border-left-color:#da291c">
-                    <span class="dato-titulo">Máquinas paradas</span>
-                    <span class="dato-valor" id="total-p" style="color:#ff9999">0</span>
-                </div>
-                <button style="background:#da291c; color:white; border:none; padding:8px 12px; border-radius:4px; margin-left:auto; cursor:pointer;" onclick="location.reload()">X</button>
+            <div class="seccion-estatus">
+                <div style="color:white; font-size:12px; font-weight:bold;">Estatus planta</div>
+                <div class="bloque-dato"><span class="dato-etiqueta">Continuas (Inact.)</span><span class="dato-valor" id="h-cont">0</span></div>
+                <div class="bloque-dato"><span class="dato-etiqueta">Open-end (Inact.)</span><span class="dato-valor" id="h-oe">0</span></div>
+                <div class="bloque-dato" style="border-left-color:#da291c"><span class="dato-etiqueta">Máquinas paradas</span><span class="dato-valor" id="total-p" style="color:#ff9999">0</span></div>
+                <button style="background:#da291c; color:white; border:none; width:25px; height:25px; border-radius:4px; margin-left:auto;" onclick="location.reload()">X</button>
             </div>`;
     }
 };
