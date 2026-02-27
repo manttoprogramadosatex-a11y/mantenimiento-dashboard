99de971428978cb6a13aaa5fb5f2d296d329e864
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 16:38:26 -0600

    Update lib_header_design.js

--- lib_header_design.js
+++ lib_header_design.js
@@ -3,14 +3,12 @@ const SatexHeaderDesign = {
         const hoy = new Date();
         const opciones = { day: 'numeric', month: 'long', year: 'numeric' };
         document.getElementById(id).innerHTML = `
-            <div style="background:#274a66; display:flex; align-items:center; height:60px; padding:0 15px; border-bottom:2px solid #1e3a50;">
-                <img src="logo.png.jpeg" style="height:45px;">
-                <h1 style="font-family:'Calibri'; font-size:26px; font-weight:bold; color:white; flex-grow:1; text-align:center; margin:0; text-transform:uppercase;">
-                    TABLERO MASTER MANTTO. SATEX TEXTIL
-                </h1>
-                <div style="color:white; font-family:sans-serif; font-size:14px; min-width:180px; text-align:right;">
-                    ${hoy.toLocaleDateString('es-ES', opciones)}
-                </div>
-            </div>`;
+        <div style="background:#274a66; display:flex; align-items:center; height:60px; padding:0 15px; border-bottom:2px solid #1e3a50;">
+            <img src="logo.png.jpeg" style="height:45px;">
+            <h1 style="font-family:'Calibri'; font-size:26px; font-weight:bold; color:white; flex-grow:1; text-align:center; margin:0; text-transform:uppercase;">TABLERO MASTER MANTTO. SATEX TEXTIL</h1>
+            <div style="color:white; font-family:sans-serif; font-size:14px; min-width:180px; text-align:right;">
+                ${hoy.toLocaleDateString('es-ES', opciones)}
+            </div>
+        </div>`;
     }
 };
