c880a902dd155e21fde4c12aef1ea672bd69fd53
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 17:20:35 -0600

    Update lib_cardas_engine.js

--- lib_cardas_engine.js
+++ lib_cardas_engine.js
@@ -4,19 +4,13 @@ const SatexCardasEngine = {
     dibujar: function(contenedorId) {
         this.grid = document.getElementById(contenedorId);
         if (!this.grid) return;
-        this.grid.style.display = "flex";
-        this.grid.style.flexWrap = "nowrap";
-        this.grid.style.overflowX = "auto";
-        this.grid.style.marginTop = "8px"; // Separación de 2mm arriba
-        this.grid.style.paddingBottom = "8px"; // Separación de 2mm abajo (Scroll)
-        this.grid.style.paddingTop = "0px";
-        this.grid.style.gap = "8px";
-        this.grid.style.width = "100%";
+        this.grid.style.marginTop = "8px"; 
+        this.grid.style.paddingBottom = "8px";
         this.grid.innerHTML = "";
         this.datosUnidades = [];
         for (let i = 1; i <= 11; i++) {
             const maxVal = 1000;
-            const acVal = Math.floor(Math.random() * 1250);
+            const acVal = Math.floor(Math.random() * 1100);
             this.datosUnidades.push({ ac: acVal, max: maxVal });
             this.grid.innerHTML += SatexCardasDesign.crearEstructura(i, acVal, maxVal);
         }
