e4995a66d1c88a99f0e37f2b640611cd76005e77
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 08:20:28 -0600

    Update lib_estatus.js

--- lib_estatus.js
+++ lib_estatus.js
@@ -1,12 +1,19 @@
 const SatexEstatus = {
     dibujar: function(id) {
         document.getElementById(id).innerHTML = `
-            <div class="seccion-estatus">
-                <div style="color:white; font-size:12px; font-weight:bold;">Estatus planta</div>
+            <div class="seccion-estatus" style="
+                padding: 0 20px; 
+                display: flex; 
+                align-items: center; 
+                height: 50px; 
+                background: rgba(0,0,0,0.15); 
+                gap: 15px;
+            ">
+                <div style="color:white; font-size:13px; font-weight:bold; text-transform:none;">Estatus planta</div>
                 <div class="bloque-dato"><span class="dato-etiqueta">Continuas (Inact.)</span><span class="dato-valor" id="h-cont">0</span></div>
                 <div class="bloque-dato"><span class="dato-etiqueta">Open-end (Inact.)</span><span class="dato-valor" id="h-oe">0</span></div>
                 <div class="bloque-dato" style="border-left-color:#da291c"><span class="dato-etiqueta">Máquinas paradas</span><span class="dato-valor" id="total-p" style="color:#ff9999">0</span></div>
-                <button style="background:#da291c; color:white; border:none; width:25px; height:25px; border-radius:4px; margin-left:auto;" onclick="location.reload()">X</button>
+                <button style="background:#da291c; color:white; border:none; width:28px; height:28px; border-radius:4px; cursor:pointer; margin-left:auto;" onclick="location.reload()">X</button>
             </div>`;
     }
 };
