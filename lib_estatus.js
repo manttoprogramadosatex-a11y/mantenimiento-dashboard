ca35379d7d12f7debb52ca21183983d6e00655ad
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-26 20:53:22 -0600

    Update lib_estatus.js

--- lib_estatus.js
+++ lib_estatus.js
@@ -1,13 +1,8 @@
-/* LIBRERÍA DE ESTATUS - PROYECTO SATEX 2026 */
 const SatexEstatus = {
-    dibujar: function(idContenedor) {
-        const html = `
+    dibujar: function(id) {
+        document.getElementById(id).innerHTML = `
             <div class="seccion-estatus">
-                <div class="etiqueta-planta">
-                    Estatus planta<br>
-                    <span style="font-size: 0.8em; opacity: 0.9;">Info. Cardas</span> ▶
-                </div>
-                
+                <div class="etiqueta-planta">Estatus planta<br><span style="font-size:0.9em; opacity:0.8;">Info. Cardas</span> ▶</div>
                 <div class="contenedor-bloques">
                     <div class="bloque-dato">
                         <span class="dato-etiqueta">Continuas (Inact.)</span>
@@ -26,16 +21,7 @@ const SatexEstatus = {
                         <span class="dato-valor" id="total-p" style="color:#ff9999">0</span>
                     </div>
                 </div>
-
-                <div class="scroll-paradas" id="listaParadas">
-                    </div>
-
-                <div class="info-sync">
-                    Actualizado:<br><span id="last-sync">Simulando...</span>
-                </div>
                 <button class="btn-refresh" onclick="location.reload()">X</button>
-            </div>
-        `;
-        document.getElementById(idContenedor).innerHTML = html;
+            </div>`;
     }
 };
