6913e9d1d32944578f5b61fc38c1ce3bcb2523d1
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 17:45:25 -0600

    Update lib_header_design.js

--- lib_header_design.js
+++ lib_header_design.js
@@ -1,3 +1,7 @@
+/**
+ * Librería: lib_header_design.js
+ * Objetivo: Encabezado con título agrandado.
+ */
 const SatexHeaderDesign = {
     render: function(id) {
         const hoy = new Date();
@@ -5,7 +9,9 @@ const SatexHeaderDesign = {
         document.getElementById(id).innerHTML = `
         <div style="background:#274a66; display:flex; align-items:center; height:60px; padding:0 15px; border-bottom:2px solid #1e3a50;">
             <img src="logo.png.jpeg" style="height:45px;">
-            <h1 style="font-family:'Calibri'; font-size:26px; font-weight:bold; color:white; flex-grow:1; text-align:center; margin:0; text-transform:uppercase;">TABLERO MASTER MANTTO. SATEX TEXTIL</h1>
+            <h1 style="font-family:'Calibri'; font-size:32px; font-weight:bold; color:white; flex-grow:1; text-align:center; margin:0; text-transform:uppercase; letter-spacing: 1px;">
+                TABLERO MASTER MANTTO. SATEX TEXTIL
+            </h1>
             <div style="color:white; font-family:sans-serif; font-size:14px; min-width:180px; text-align:right;">
                 ${hoy.toLocaleDateString('es-ES', opciones)}
             </div>
