a18cbba8890c893fcd402feda6df9af188ed6871
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 08:07:08 -0600

    Update lib_header.js

--- lib_header.js
+++ lib_header.js
@@ -1,12 +1,11 @@
 const SatexHeader = {
     dibujar: function(id) {
         const hoy = new Date();
-        const fecha = hoy.toLocaleDateString('es-ES', { day: 'numeric', month: 'long', year: 'numeric' });
         document.getElementById(id).innerHTML = `
             <div class="header-satex">
-                <img src="logo.png.jpeg" alt="Logo">
-                <div class="titulo-master">TABLERO MASTER MANTTO. SATEX TEXTIL</div>
-                <div class="fecha-txt">${fecha}</div>
+                <img src="logo.png.jpeg" alt="Logo" style="height:40px;">
+                <div class="titulo-principal">TABLERO MASTER MANTTO. SATEX TEXTIL</div>
+                <div style="font-size:13px; min-width:150px; text-align:right;">${hoy.toLocaleDateString()}</div>
             </div>`;
     }
 };
