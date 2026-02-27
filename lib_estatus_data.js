b1ee6f3219831065f22589f48addf18b2553aad0
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 09:38:32 -0600

    Update lib_estatus_data.js

--- lib_estatus_data.js
+++ lib_estatus_data.js
@@ -1,10 +1,9 @@
 const SatexEstatusData = {
     actualizar: function(hCont, hOe, hCon, mParadas) {
         const container = document.getElementById('data-husos-container');
-        
         const crearBloque = (label, valor, color = "#f9b218") => `
-            <div style="background: #3c546d; border-left: 3px solid ${color}; padding: 2px 10px; min-width: 100px; height: 40px; display: flex; flex-direction: column; justify-content: center; box-sizing: border-box;">
-                <span style="color: #a1b1c1; font-size: 9px; font-weight: bold; font-family: sans-serif; white-space: nowrap; text-transform: none;">${label}</span>
+            <div style="background: #3c546d; border-left: 3px solid ${color}; padding: 2px 10px; min-width: 105px; height: 40px; display: flex; flex-direction: column; justify-content: center; box-sizing: border-box;">
+                <span style="color: #a1b1c1; font-size: 9px; font-weight: bold; font-family: sans-serif; white-space: nowrap;">${label}</span>
                 <span style="color: ${color}; font-size: 20px; font-weight: bold; font-family: sans-serif; line-height: 1;">${valor}</span>
             </div>`;
         
@@ -12,6 +11,6 @@ const SatexEstatusData = {
             crearBloque("Continuas (Inact.)", hCont) +
             crearBloque("Open-end (Inact.)", hOe) +
             crearBloque("Coneras (Inact.)", hCon) +
-            crearBloque("Máquinas paradas", mParadas, "#da291c"); // Recuperado bloque de máquinas paradas en rojo
+            crearBloque("Máquinas paradas", mParadas, "#da291c");
     }
 };
