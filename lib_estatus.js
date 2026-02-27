90697e455dce3d80bcd53c0587d50ab05c26852c
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-26 20:50:33 -0600

    Update lib_estatus.js

--- lib_estatus.js
+++ lib_estatus.js
@@ -1,34 +1,41 @@
+/* LIBRERÍA DE ESTATUS - PROYECTO SATEX 2026 */
 const SatexEstatus = {
-    dibujar: function(id) {
-        document.getElementById(id).innerHTML = `
+    dibujar: function(idContenedor) {
+        const html = `
             <div class="seccion-estatus">
-                <div class="etiqueta-planta">ESTATUS<br>PLANTA ▶</div>
+                <div class="etiqueta-planta">
+                    Estatus planta<br>
+                    <span style="font-size: 0.8em; opacity: 0.9;">Info. Cardas</span> ▶
+                </div>
+                
                 <div class="contenedor-bloques">
                     <div class="bloque-dato">
-                        <span class="dato-etiqueta">HUSOS CONTINUAS (INACT.)</span>
-                        <span class="dato-valor" id="h-cont">20</span>
+                        <span class="dato-etiqueta">Continuas (Inact.)</span>
+                        <span class="dato-valor" id="h-cont">0</span>
                     </div>
                     <div class="bloque-dato">
-                        <span class="dato-etiqueta">HUSOS OPEN-END (INACT.)</span>
-                        <span class="dato-valor" id="h-oe">4</span>
+                        <span class="dato-etiqueta">Open-End (Inact.)</span>
+                        <span class="dato-valor" id="h-oe">0</span>
                     </div>
                     <div class="bloque-dato">
-                        <span class="dato-etiqueta">HUSOS CONERAS (INACT.)</span>
+                        <span class="dato-etiqueta">Coneras (Inact.)</span>
                         <span class="dato-valor" id="h-con">0</span>
                     </div>
                     <div class="bloque-dato box-paradas">
-                        <span class="dato-etiqueta">MÁQUINAS PARADAS</span>
-                        <span class="dato-valor" id="total-p" style="color:#ff9999">2</span>
+                        <span class="dato-etiqueta">Máquinas Paradas</span>
+                        <span class="dato-valor" id="total-p" style="color:#ff9999">0</span>
                     </div>
                 </div>
+
                 <div class="scroll-paradas" id="listaParadas">
-                    <div class="item-parada"><strong>Regulador 10</strong><div class="dias-badge">21 Días</div></div>
-                    <div class="item-parada"><strong>Regulador 2</strong><div class="dias-badge">0 Días</div></div>
-                </div>
-                <div style="color: #00d4ff; font-size: 10px; font-weight: bold; text-align: right; margin-left: auto;">
-                    Actualizado:<br><span id="last-sync">Sincronizado</span>
+                    </div>
+
+                <div class="info-sync">
+                    Actualizado:<br><span id="last-sync">Simulando...</span>
                 </div>
                 <button class="btn-refresh" onclick="location.reload()">X</button>
-            </div>`;
+            </div>
+        `;
+        document.getElementById(idContenedor).innerHTML = html;
     }
 };
