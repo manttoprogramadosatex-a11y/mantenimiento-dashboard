2ef6f4d3c509da32498113eafcdf70bb79bb901a
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 16:35:44 -0600

    Update lib_cardas.js

--- lib_cardas.js
+++ lib_cardas.js
@@ -2,15 +2,12 @@ const SatexCardas = {
     inicializarIndicador: function(id, actual, maximo) {
         const ctx = document.getElementById(id);
         if (!ctx) return;
-
         const porc = (actual / maximo) * 100;
-        let col = '#28a745'; 
-        if (porc > 100) col = '#da291c'; 
+        let col = '#28a745';
+        if (porc > 100) col = '#da291c';
         else if (porc > 80) col = '#f9b218';
-
         const chartExistente = Chart.getChart(id);
         if (chartExistente) { chartExistente.destroy(); }
-
         new Chart(ctx, {
             type: 'doughnut',
             data: {
@@ -25,7 +22,7 @@ const SatexCardas = {
             options: {
                 responsive: true,
                 maintainAspectRatio: false,
-                layout: { padding: 0 }, // Elimina espacio interno del gráfico
+                layout: { padding: 0 },
                 cutout: '75%',
                 plugins: { legend: { display: false }, tooltip: { enabled: false } }
             }
