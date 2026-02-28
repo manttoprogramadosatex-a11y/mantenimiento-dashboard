90b8f74e5f8fe4f1023f0765bd3d4fed542fc593
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 19:36:07 -0600

    Update lib_mantenimiento.js

--- lib_mantenimiento.js
+++ lib_mantenimiento.js
@@ -4,15 +4,16 @@ const SatexMantenimiento = {
         const container = document.getElementById(id);
         if (!container) return;
         
-        // Ajuste de altura del contenedor para albergar el espacio de 6.5cm
+        // Mantenemos el pegado estricto de 1mm
         container.style.marginTop = "0px";
         container.style.paddingTop = "0px";
-        container.style.minHeight = "450px"; 
+        // Aumentamos el mínimo para que quepa la nueva distancia
+        container.style.minHeight = "500px"; 
 
         container.innerHTML = `
             <div style="display: flex; width: 100%; height: 100%; position: relative; font-family: 'Segoe UI', sans-serif; margin-top: 4px;">
                 
-                <div style="position: absolute; left: 50%; top: 0px; height: 420px; width: 2px; background-color: #f9b218; transform: translateX(-50%); z-index: 5;"></div>
+                <div style="position: absolute; left: 50%; top: 0px; height: 460px; width: 2px; background-color: #f9b218; transform: translateX(-50%); z-index: 5;"></div>
                 
                 <div style="flex: 1; display: flex; flex-direction: column; align-items: center;">
                     <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 85%; text-align: center; text-transform: uppercase;">
@@ -26,7 +27,7 @@ const SatexMantenimiento = {
                         Mantto. Preventivo
                     </div>
                     
-                    <div style="height: 245px;"></div>
+                    <div style="height: 283px;"></div>
                     
                     <div style="color: #ff9999; font-size: 14px; font-weight: bold; border-bottom: 1px solid #ff9999; padding-bottom: 2px; width: 70%; text-align: center; text-transform: uppercase;">
                         Accidentes
