ee89f5f84974abe9cfeddbf3515d911a8043a492
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-28 10:43:39 -0600

    Update lib_mantenimiento.js

--- lib_mantenimiento.js
+++ lib_mantenimiento.js
@@ -13,7 +13,7 @@ const SatexMantenimiento = {
             <div style="flex: 1; display: flex; flex-direction: column; align-items: center; position: relative; padding-top: 5px;">
                 <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 85%; text-align: center; text-transform: uppercase;">Mantto. Preventivo</div>
                 
-                <div id="preventivos-grafico-container" style="width: 100%; height: 110px; margin-top: 10px;"></div>
+                <div id="preventivos-grafico-container" style="width: 100%; height: calc(100% - 150px); display: flex; align-items: center; justify-content: center; margin-top: 10px;"></div>
 
                 <div id="titulo-accidentes" style="position: absolute; bottom: 85px; left: 50%; transform: translateX(-50%); color: #ff9999; font-size: 14px; font-weight: bold; border-bottom: 1px solid #ff9999; padding-bottom: 2px; width: 70%; text-align: center; text-transform: uppercase;">Accidentes</div>
                 <div id="accidentes-scroll" style="position: absolute; bottom: 10px; left: 5%; width: 90%; height: 65px; background: rgba(0,0,0,0.2); border-radius: 4px; padding: 5px; box-sizing: border-box; overflow: hidden;"></div>
