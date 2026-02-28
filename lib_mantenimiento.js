f53f99937d2e2aa9f26e5e933172a8e8cc136c5f
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 20:25:08 -0600

    Update lib_mantenimiento.js

--- lib_mantenimiento.js
+++ lib_mantenimiento.js
@@ -4,21 +4,18 @@ const SatexMantenimiento = {
         const container = document.getElementById(id);
         if (!container) return;
         
-        container.style.width = "100%";
-        container.style.height = "100%";
-
         container.innerHTML = `
-            <div style="display: flex; width: 100%; height: 100%; position: relative; font-family: 'Segoe UI', sans-serif;">
+            <div style="display: flex; width: 100%; height: 100%; position: relative; font-family: 'Segoe UI', sans-serif; margin-top: 5px;">
                 
-                <div style="position: absolute; left: 50%; top: 0px; bottom: 10px; width: 2px; background-color: #f9b218; transform: translateX(-50%); z-index: 5;"></div>
+                <div style="position: absolute; left: 50%; top: 0px; bottom: 20px; width: 2px; background-color: #f9b218; transform: translateX(-50%); z-index: 5;"></div>
                 
-                <div style="flex: 1; display: flex; flex-direction: column; align-items: center; padding-top: 5px;">
+                <div style="flex: 1; display: flex; flex-direction: column; align-items: center;">
                     <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 85%; text-align: center; text-transform: uppercase;">
                         Mantto. Correctivo
                     </div>
                 </div>
                 
-                <div style="flex: 1; display: flex; flex-direction: column; align-items: center; position: relative; padding-top: 5px;">
+                <div style="flex: 1; display: flex; flex-direction: column; align-items: center; position: relative;">
                     
                     <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 85%; text-align: center; text-transform: uppercase;">
                         Mantto. Preventivo
