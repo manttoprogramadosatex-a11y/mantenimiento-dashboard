bd09c8f31c7a25ac02c88105938c22bf604141fd
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-28 08:09:16 -0600

    Update lib_maquinas_inactivas.js

--- lib_maquinas_inactivas.js
+++ lib_maquinas_inactivas.js
@@ -1,4 +1,3 @@
-/* lib_maquinas_inactivas.js */
 const SatexMaquinasInactivas = {
     render: function(id, datos) {
         const container = document.getElementById(id);
@@ -8,7 +7,7 @@ const SatexMaquinasInactivas = {
             <div style="display: grid; grid-template-columns: 1fr 1fr 1.5fr 1.2fr; color: white; font-size: 12px; border-bottom: 1px solid rgba(255,255,255,0.05); padding: 5px 0; align-items: center;">
                 <span style="font-weight: bold;">${m.tipo}</span>
                 <span style="color: #ff9999; font-weight: bold; text-align: center;">${m.num}</span>
-                <span style="text-align: center; opacity: 0.9;">${m.fecha}</span>
+                <span style="text-align: center;">${m.fecha}</span>
                 <span style="color: #f9b218; text-align: right; font-weight: bold; padding-right: 5px;">${m.dias}d</span>
             </div>`).join('');
 
