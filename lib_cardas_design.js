dde0fe2829e6fe369882f0a48a78ba26dc2f58a9
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-27 21:38:49 -0600

    Update lib_cardas_design.js

--- lib_cardas_design.js
+++ lib_cardas_design.js
@@ -2,10 +2,10 @@
 const SatexCardasDesign = {
     crearCarda: function(id, titulo, actual, maximo) {
         return `
-        <div id="carda-${id}" style="min-width: 140px; height: 185px; background: white; border-radius: 8px; margin: 0 5px; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 5px; box-shadow: 0 4px 6px rgba(0,0,0,0.3); border: 1px solid #2f5577; box-sizing: border-box;">
-            <div style="color: #003366; font-weight: bold; font-size: 13px; margin-bottom: 5px; text-transform: uppercase;">${titulo}</div>
-            <canvas id="canvas-${id}" width="115" height="80"></canvas>
-            <div style="margin-top: 5px; text-align: center;">
+        <div id="carda-${id}" style="min-width: 135px; height: 185px; background: white; border-radius: 8px; margin: 0 5px; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 5px; box-shadow: 0 2px 4px rgba(0,0,0,0.3); border: 1px solid #2f5577; box-sizing: border-box;">
+            <div style="color: #003366; font-weight: bold; font-size: 12px; margin-bottom: 3px; text-transform: uppercase;">${titulo}</div>
+            <canvas id="canvas-${id}" width="115" height="75"></canvas>
+            <div style="margin-top: 3px; text-align: center;">
                 <div style="font-size: 12px; font-weight: bold; color: #333;">Ac. ${actual}</div>
                 <div style="font-size: 12px; font-weight: bold; color: #cc0000;">Max. ${maximo}</div>
             </div>
