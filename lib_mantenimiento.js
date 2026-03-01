a920b0b5fbe9a2df8516583f83d9c22793ab5e10
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-28 19:19:52 -0600

    Update lib_mantenimiento.js

--- lib_mantenimiento.js
+++ lib_mantenimiento.js
@@ -9,7 +9,7 @@ const SatexMantenimiento = {
             <div style="position: absolute; left: 50%; top: 0; bottom: 0; width: 2px; background-color: #f9b218; transform: translateX(-50%); z-index: 5;"></div>
             
             <div style="flex: 1; display: flex; flex-direction: column; padding: 0px 15px 5px 15px;">
-                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px; margin-top: 4px;">
+                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px; margin-top: 0px;">
                     Mantto. Correctivo
                 </div>
                 
@@ -40,7 +40,7 @@ const SatexMantenimiento = {
             </div>
 
             <div style="flex: 1; display: flex; flex-direction: column; padding: 0px 15px 5px 15px;">
-                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px; margin-top: 4px;">
+                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px; margin-top: 0px;">
                     Mantto. Preventivo
                 </div>
                 
