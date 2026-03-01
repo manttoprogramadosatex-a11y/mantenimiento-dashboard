9fd069a56103ee5b3b06f82115141aa8304b1f2d
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-03-01 08:01:28 -0600

    Update lib_header.js

--- lib_header.js
+++ lib_header.js
@@ -1,9 +1,8 @@
 /* lib_header.js */
-/* VERSION 1.1.2
-   - Logo desplazado visualmente 1cm a la derecha
-   - Título permanece centrado real
-   - Fecha no se mueve
-   - Altura 60px intacta
+/* VERSION 1.1
+   - Se agrega subtítulo: MONITOREO CORRECTIVOS EN TIEMPO REAL
+   - No se modifica altura (60px)
+   - No se altera layout general
 */
 
 const SatexHeader = {
@@ -14,17 +13,14 @@ const SatexHeader = {
         document.getElementById(id).innerHTML = `
             <div style="background:#274a66; display:flex; align-items:center; height:60px; padding:0 15px; border-bottom:2px solid #1e3a50;">
                 
-                <!-- Contenedor fijo del logo -->
-                <div style="width:200px; display:flex; justify-content:flex-start;">
-                    <img src="logo.png.jpeg" style="height:45px; margin-left:35px;">
-                </div>
+                <img src="logo.png.jpeg" style="height:45px;">
 
                 <h1 style="
                     font-family:'Calibri';
                     font-size:26px;
                     font-weight:bold;
                     color:white;
-                    flex:1;
+                    flex-grow:1;
                     text-align:center;
                     margin:0;
                     text-transform:uppercase;
@@ -33,8 +29,8 @@ const SatexHeader = {
                 </h1>
 
                 <div style="
-                    min-width:200px;
                     color:white;
+                    min-width:200px;
                     text-align:right;
                     line-height:1.1;
                 ">
