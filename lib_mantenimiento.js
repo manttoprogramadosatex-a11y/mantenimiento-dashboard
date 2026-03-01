cd7d888fd8a1b7ce3f48d7d9ab61d8104d32b88c
manttoprogramadosatex-a11y <mantto.programado.satex@gmail.com>
2026-02-28 19:14:46 -0600

    Update lib_mantenimiento.js

--- lib_mantenimiento.js
+++ lib_mantenimiento.js
@@ -8,8 +8,10 @@ const SatexMantenimiento = {
         <div style="display: flex; width: 100%; height: 100%; position: relative; font-family: 'Segoe UI', sans-serif;">
             <div style="position: absolute; left: 50%; top: 0; bottom: 0; width: 2px; background-color: #f9b218; transform: translateX(-50%); z-index: 5;"></div>
             
-            <div style="flex: 1; display: flex; flex-direction: column; padding: 5px 15px;">
-                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px;">Mantto. Correctivo</div>
+            <div style="flex: 1; display: flex; flex-direction: column; padding: 2px 15px 5px 15px;">
+                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px; margin-top: 0px;">
+                    Mantto. Correctivo
+                </div>
                 
                 <div style="display: flex; width: 100%; height: 160px; gap: 10px; margin-bottom: 10px;">
                     <div style="width: 50%; display: flex; gap: 5px;">
@@ -37,8 +39,11 @@ const SatexMantenimiento = {
                 </div>
             </div>
 
-            <div style="flex: 1; display: flex; flex-direction: column; padding: 5px 15px;">
-                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px;">Mantto. Preventivo</div>
+            <div style="flex: 1; display: flex; flex-direction: column; padding: 2px 15px 5px 15px;">
+                <div style="color: #ffffff; font-size: 18px; font-weight: bold; border-bottom: 2px solid #f9b218; padding-bottom: 2px; width: 100%; text-align: center; text-transform: uppercase; margin-bottom: 4px; margin-top: 0px;">
+                    Mantto. Preventivo
+                </div>
+                
                 <div id="preventivos-grafico-container" style="width: 100%; height: 180px;"></div>
                 
                 <div style="margin-top: auto; padding-bottom: 10px;">
